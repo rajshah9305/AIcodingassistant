88bf3e02459e5c1f6cdcbaad68250f9f16deb050
Raj Shah <rajshah9305@gmail.com>
2025-11-23 23:21:46 -0700

    Fix Vercel API handler types

--- api/trpc/[...].ts
+++ api/trpc/[...].ts
@@ -1,13 +1,14 @@
 import "dotenv/config";
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
 import { appRouter } from "../../server/router";
+import type { Request as ExpressRequest, Response as ExpressResponse } from "express";
 
-export default async function handler(req: Request): Promise<Response> {
+export default async function handler(req: ExpressRequest, res: ExpressResponse) {
   return fetchRequestHandler({
     endpoint: "/api/trpc",
-    req,
+    req: req as any,
     router: appRouter,
-    createContext: () => ({}),
+    createContext: () => ({ req, res }),
     onError: ({ error, path }) => {
       console.error(`tRPC error on '${path}':`, error);
     },
